6b6e3f214e9362c455589731868fc0ff928acdc7
Phylicia Campbell <phylicia.campbell@sckans.edu>
2018-09-16 15:38:46 -0500

    Responsive Design Update
    Styled Navigation Bar and created mobile menu. Made Navigation Bar and
    header section responsive to various screen sizes.

---
+++ Landing/index.js
@@ -0,0 +1,24 @@
+let toggleBtn = document.getElementById("toggle");
+let navContainer = document.getElementById("nav-container");
+let toggle = false;
+
+
+toggleBtn.addEventListener('click', function(){
+//function to hide nav links
+    if(toggle === false)
+    {
+        navContainer.style.display = "none";
+        toggle = true;
+    } else{
+        navContainer.style.display = "flex";
+        toggle = false;
+    }
+
+});
+
+
+//hides nav bar on page load and displays as user scrolls
+hideNavBar = () => {
+    let navBar = document.getElementById("nav-bar");
+
+}
